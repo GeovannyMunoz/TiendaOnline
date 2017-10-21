b9ecbcab64acc235f7adf4deba4ef32b1de1fb8b
GeovannyMunoz <geo_roskax100pre@hotmail.com>
2017-10-21 18:41:46 -0500

    Tienda Online base

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+
+//RUTAS
+import { APP_ROUTING } from './app.routes';
+
+//SERVICIOS
+
+//COMPONENTES
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
